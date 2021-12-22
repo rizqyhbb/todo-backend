a13c3f9d83046fd37a03bf79ce3137bcd6700e72
rizqyhbb <rizqyhbb@gmail.com>
2021-12-22 16:49:08 +0700

    adding new config for DB production

--- src/config/config.js
+++ src/config/config.js
@@ -18,11 +18,11 @@ module.exports = {
     port: process.env.DB_PORT
   },
   "production": {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    host: process.env.DB_HOST,
+    username: process.env.DB_PRODUCTION_USERNAME,
+    password: process.env.DB_PRODUCTION_PASSWORD,
+    database: process.env.DB_PRODUCTION_DATABASE,
+    host: process.env.DB_PRODUCTION_HOST,
     dialect: 'postgres',
-    port: process.env.DB_PORT
+    port: process.env.DB_PRODUCTION_PORT
   }
 }
