715b7e8e012452e4596c314e737b49d6864c1b18
rizqyhbb <rizqyhbb@gmail.com>
2021-12-23 18:20:38 +0700

    change origin cors

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import cors from 'cors'
 const app = express()
 require('dotenv').config()
 
-app.use(cors({origin: 'http://localhost:3001'} ))
+app.use(cors({origin: 'http://localhost:3000'} ))
 app.use(express.json());
 app.use('/api', apis)
 
