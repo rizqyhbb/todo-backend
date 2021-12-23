78283a7d57cb6b7200c94a7b55d3b1be34801699
rizqyhbb <rizqyhbb@gmail.com>
2021-12-23 13:14:31 +0700

    hehe

--- src/models/index.js
+++ src/models/index.js
@@ -25,7 +25,7 @@ let sequelize;
 //   sequelize = new Sequelize(config.database, config.username, config.password, config);
 // }
 if (config.use_env_variable) {
-  sequelize = new Sequelize(config.use_env_variable, config);
+  sequelize = new Sequelize(config.use_env_variable, null, null);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
