6bbbc7c77ce2858ae4358a195bb8cf08613eecc1
rizqyhbb <rizqyhbb@gmail.com>
2021-12-23 12:33:45 +0700

    new config

--- src/config/config.js
+++ src/config/config.js
@@ -18,14 +18,17 @@ module.exports = {
     port: process.env.DB_PORT
   },
   production: {
-    use_env_variable: 'DATABASE_URL',
-    dialect: 'postgres',
-    protocol: 'postgres',
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
-    },
-  },
+    use_env_variable: 'DATABSE_URL'
+  }
+  // production: {
+  //   use_env_variable: 'DATABASE_URL',
+  //   dialect: 'postgres',
+  //   protocol: 'postgres',
+  //   dialectOptions: {
+  //     ssl: {
+  //       require: true,
+  //       rejectUnauthorized: false,
+  //     },
+  //   },
+  // },
 }
